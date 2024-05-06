37885c9e033f7f28fd229d26be372ca75259a5c7
HanaokaY <okayu5757@ezweb.ne.jp>
2024-05-06 10:06:40 +0900

    プロダクト画面

---
+++ src/views/Products.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>商品一覧</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['products'])
+  },
+  created() {
+    this.$store.dispatch('fetchProducts');
+  }
+}
+</script>
